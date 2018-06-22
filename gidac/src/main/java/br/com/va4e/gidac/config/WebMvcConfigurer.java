9df91a46a31e2d0b8924510593ebac255dddd0bc
dullio <dullio@va4e-dell>
2018-06-22 00:04:16 -0300

    thymeleaf

---
+++ gidac/src/main/java/br/com/va4e/gidac/config/WebMvcConfigurer.java
@@ -0,0 +1,154 @@
+package br.com.va4e.gidac.config;
+
+import java.util.Locale;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+	
+	
+	
+	
+	// private ApplicationContext applicationContext;
+
+
+	    public WebMvcConfigurer() {
+	        super();
+	    }
+
+/*
+	    public void setApplicationContext(final ApplicationContext applicationContext)
+	            throws BeansException {
+	        applicationContext = applicationContext;
+	    }
+
+*/
+
+	    /* ******************************************************************* */
+	    /*  GENERAL CONFIGURATION ARTIFACTS                                    */
+	    /*  Static Resources, i18n Messages, Formatters (Conversion Service)   */
+	    /* ******************************************************************* */
+
+	    @Override
+	    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	        super.addResourceHandlers(registry);
+	        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+	        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+	        registry.addResourceHandler("/js/**").addResourceLocations("/js/")
+	        .resourceChain(true)
+	        .addResolver(new WebJarsResourceResolver());
+
+	    }
+
+	    @Bean
+	    ReloadableResourceBundleMessageSource messageSource() {
+	        ReloadableResourceBundleMessageSource bundleMessageSource = new ReloadableResourceBundleMessageSource();
+	        bundleMessageSource.setBasename("classpath:i18n/messages");
+	        bundleMessageSource.setCacheSeconds(1800);
+	        bundleMessageSource.setDefaultEncoding("UTF-8");
+	        return bundleMessageSource;
+	    }
+
+
+	    /* **************************************************************** */
+	    /*  THYMELEAF-SPECIFIC ARTIFACTS                                    */
+	    /*  TemplateResolver <- TemplateEngine <- ViewResolver              */
+	    /* **************************************************************** */
+
+	    @Bean
+	    public SpringResourceTemplateResolver templateResolver(){
+
+	        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	        
+	       // templateResolver.setApplicationContext(this.applicationContext);
+	        
+	        templateResolver.setPrefix("/WEB-INF/templates/");
+	        templateResolver.setSuffix(".html");
+	        //templateResolver.setTemplateMode(TemplateMode.HTML);
+	        // Template cache is true by default. Set to false if you want
+	        // templates to be automatically updated when modified.
+	        //templateResolver.setCacheable(true);
+	        return templateResolver;
+	    }
+
+	    @Bean
+	    public SpringTemplateEngine templateEngine(){
+	        // SpringTemplateEngine automatically applies SpringStandardDialect and
+	        // enables Spring's own MessageSource message resolution mechanisms.
+	        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	        templateEngine.setTemplateResolver(templateResolver());
+	        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+	        // speed up execution in most scenarios, but might be incompatible
+	        // with specific cases when expressions in one template are reused
+	        // across different data types, so this flag is "false" by default
+	        // for safer backwards compatibility.
+	       // templateEngine.setEnableSpringELCompiler(true);
+	        
+	        return templateEngine;
+	    }
+
+	    @Bean
+	    public ThymeleafViewResolver viewResolver(){
+	        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	        viewResolver.setTemplateEngine(templateEngine());
+	        return viewResolver;
+	    }
+
+	    @Bean
+	    public LocaleResolver localeResolver(){
+	        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+	        sessionLocaleResolver.setDefaultLocale(Locale.US);
+	        return sessionLocaleResolver;
+	    }
+
+	    @Bean
+	    LocaleChangeInterceptor localeChangeInterceptor(){
+	        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+	        localeChangeInterceptor.setParamName("lang");
+	        return localeChangeInterceptor;
+	    }
+
+	    @Override
+	    public void addInterceptors(InterceptorRegistry interceptorRegistry){
+	        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+	    }
+
+		@Override
+		public void setApplicationContext(org.springframework.context.ApplicationContext applicationContext)
+				throws BeansException {
+			// TODO Auto-generated method stub
+			
+		}
+	
+
+}
+
+
+
+
+   
+
+
+
+
+
---
+++ gidac/src/main/java/br/com/va4e/gidac/entity/Member.java
@@ -0,0 +1,170 @@
+
+package br.com.va4e.gidac.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import br.com.va4e.gidac.models.AuditModel;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "firstName", "lastName" }), name = "members")
+public class Member extends AuditModel implements Serializable {
+
+	public Member(String firstName, String lastName, String userName, String cpf, String rg, String gender, String note,
+			boolean active, Date birthday) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.gender = gender;
+		this.note = note;
+		this.active = active;
+		this.birthday = birthday;
+	}
+
+	public Member() {
+
+	}
+
+	/**
+	 * Default value included to remove warning. Remove or modify at will. *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min = 2, max = 25)
+	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	private String firstName;
+
+	@NotNull
+	@Size(min = 2, max = 25)
+	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	private String lastName;
+
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Za-z]*", message = "must contain only letters")
+	private String userName;
+
+	private String cpf;
+
+	private String rg;
+
+	// TODO: Change to enum;
+	private String gender;
+
+	// TODO: Change data type to "memo"
+
+	private String note;
+
+	@NotNull
+	private boolean active;
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/*
+	 * //@Override public String toString() { return "Member [id=" + id +
+	 * ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" +
+	 * userName + "]"; }
+	 */
+
+}
